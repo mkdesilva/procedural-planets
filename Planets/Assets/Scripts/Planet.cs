9f2dcc9488b15a0855ef99443ded389d2b288cef
MK de Silva <dev.mkdesilva@gmail.com>
2020-05-01 21:48:32 +0700

    Added basic sphere

---
+++ Planets/Assets/Scripts/Planet.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [Range(2, 256)] public int resolution = 10;
+    [SerializeField, HideInInspector] private MeshFilter[] meshFilters;
+    private TerrainFace[] terrainFaces;
+
+    private void OnValidate()
+    {
+        Initialize();
+        GenerateMesh();
+    }
+
+    private void Initialize()
+    {
+        const int numOfFaces = 6;
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[numOfFaces];
+        }
+
+        terrainFaces = new TerrainFace[6];
+
+        Vector3[] directions = {Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back,};
+
+        for (var i = 0; i < numOfFaces; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                var meshObj = new GameObject("Mesh");
+                meshObj.transform.parent = transform;
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
+
+            terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+    }
+
+    private void GenerateMesh()
+    {
+        foreach (var face in terrainFaces)
+        {
+            face.ConstructMesh();
+        }
+    }
+}
---
+++ Planets/Assets/Scripts/TerrainFace.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainFace
+{
+    private Mesh mesh;
+    private int resolution;
+    private Vector3 localUp;
+
+    private Vector3 axisA;
+    private Vector3 axisB;
+
+    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+    {
+        this.mesh = mesh;
+        this.resolution = resolution;
+        this.localUp = localUp;
+
+        axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+        axisB = Vector3.Cross(localUp, axisA);
+    }
+
+    /// <summary>
+    /// /
+    /// _
+    /// </summary>
+    public void ConstructMesh()
+    {
+        var vertices = new Vector3[resolution * resolution];
+        var triangles = new int[(resolution - 1) * (resolution - 1) * 6];
+
+        int triIndex = 0;
+        for (var y = 0; y < resolution; y++)
+        {
+            for (var x = 0; x < resolution; x++)
+            {
+                var vertexIndex = x + y * resolution;
+                var percent = new Vector2(x, y) / (resolution - 1);
+                var pointOnUnitCube = localUp + (percent.x - 0.5f) * 2 * axisA + (percent.y - 0.5f) * 2 * axisB;
+                var pointOnUnitSphere = pointOnUnitCube.normalized;
+                
+                vertices[vertexIndex] = pointOnUnitSphere;
+
+                if (x != resolution - 1 && y != resolution - 1)
+                {
+                    triangles[triIndex] = vertexIndex;
+                    triangles[triIndex + 1] = vertexIndex + resolution + 1;
+                    triangles[triIndex + 2] = vertexIndex + resolution;
+
+                    triangles[triIndex + 3] = vertexIndex;
+                    triangles[triIndex + 4] = vertexIndex + 1;
+                    triangles[triIndex + 5] = vertexIndex + resolution + 1;
+                    triIndex += 6;
+                }
+            }
+        }
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
